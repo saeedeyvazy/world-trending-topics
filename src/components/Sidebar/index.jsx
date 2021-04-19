bb28884d8fdcdfca27767e89f09765ddd654f693
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-04-19 16:28:16 +0430

    Add image for every menu in sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,16 +1,66 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import { IMAGES } from '../../constants'
 const SidebarContainer = styled.div`
 	width: 70px;
 	height: 100%;
 	background-color: white;
 	border-right: 1px solid #c8ced596;
 	display: flex;
+	justify-content: space-between;
+	flex-direction: column;
+	align-items: center;
+	img {
+		width: 50px;
+		height: 50px;
+		cursor: pointer;
+		transition: all 200ms ease-in-out;
+		&:hover {
+			opacity: 0.7;
+		}
+	}
+`
+const SettingContainer = styled.div`
+	display: flex;
+	flex: 1;
+	justify-content: center;
+	flex-direction: column;
+	img {
+		width: 30px;
+		height: 30px;
+	}
+`
+const EmptyColumn = styled.div`
+	display: flex;
+	flex: 1;
+`
+
+const OtherMenuContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
+	flex: 3;
 `
 
 function Sidebar() {
-	return <SidebarContainer></SidebarContainer>
+	return (
+		<SidebarContainer>
+			<EmptyColumn />
+			<OtherMenuContainer>
+				<img src={IMAGES.HOME} alt='home' />
+				<img src={IMAGES.VIDEO} alt='video-message' />
+				<img src={IMAGES.EVENT} alt='events' />
+				<img src={IMAGES.FRIEND} alt='friend' />
+				<img src={IMAGES.SAVE} alt='save' />
+				<img src={IMAGES.TV} alt='tv' />
+				<img src={IMAGES.LIKE} alt='like' />
+			</OtherMenuContainer>
+			<SettingContainer>
+				<img src={IMAGES.SETTING} alt='settings' />
+			</SettingContainer>
+		</SidebarContainer>
+	)
 }
 
 export default Sidebar
---
+++ src/constants/images.js
@@ -0,0 +1,10 @@
+import SETTING from '../assets/images/icons8-settings-100.png'
+import HOME from '../assets/images/icons8-home-page-100.png'
+import VIDEO from '../assets/images/icons8-video-message-100.png'
+import EVENT from '../assets/images/icons8-important-event-100.png'
+import LIKE from '../assets/images/icons8-heart-100.png'
+import FRIEND from '../assets/images/icons8-hang-10-100.png'
+import SAVE from '../assets/images/icons8-save-100.png'
+import TV from '../assets/images/icons8-retro-tv-150.png'
+
+export { SETTING, HOME, VIDEO, EVENT, LIKE, FRIEND, SAVE, TV }
