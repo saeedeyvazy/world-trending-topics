7cb8f808f157ef2c79a65f98d5965a802595aef0
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-04-22 13:44:06 +0430

    FIX StorieSection avatar style :lipstick:

---
+++ src/components/Profile/ProfileTopSection.jsx
@@ -0,0 +1,113 @@
+import React from 'react'
+import styled from 'styled-components'
+import { IMAGES } from '../../constants'
+import { Marginer } from '../Marginer'
+
+const EditButton = styled.button`
+	background-color: black;
+	color: white;
+	border-radius: 5px;
+	width: 75px;
+	height: 35px;
+	cursor: pointer;
+	transition: all 200ms ease-in-out;
+	&:hover {
+		opacity: 0.8;
+	}
+`
+const ProfileStateContainer = styled.div`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`
+
+const ProfileState = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	padding: 15px;
+	h3 {
+		font-weight: 700;
+		font-size: 17px;
+	}
+	h4 {
+		color: gray;
+		font-size: 12px;
+		font-weight: 500;
+		padding: 5px;
+	}
+`
+const CircleAvatar = styled.div`
+	overflow: hidden;
+	border-radius: 50px;
+	width: 100%;
+	height: 100%;
+	img {
+		width: 100%;
+		height: 100%;
+	}
+`
+const ProfileName = styled.h3`
+	font-weight: 600;
+	font-size: 16px;
+	color: black;
+	padding: 10px;
+`
+const ProfileDesc = styled.h4`
+	color: gray;
+	font-size: 12px;
+	font-weight: 400;
+`
+const AvatarContainer = styled.div`
+	width: 80px;
+	height: 80px;
+	overflow: hidden;
+	border-radius: 50px;
+	background-image: linear-gradient(
+		90deg,
+		rgba(63, 94, 251, 1) 0%,
+		rgba(252, 70, 107, 1) 100%
+	);
+	padding: 2px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+
+function ProfileTopSection() {
+	return (
+		<>
+			{' '}
+			<AvatarContainer>
+				<CircleAvatar>
+					<img src={IMAGES.PROFILE_IMAGE} alt='profile' />
+				</CircleAvatar>
+			</AvatarContainer>
+			<ProfileName>Saeed Eyvazy</ProfileName>
+			<ProfileDesc>profile description</ProfileDesc>
+			<Marginer direction='vertical' margin={8} />
+			<EditButton>Edit</EditButton>
+			<Marginer direction='vertical' margin={20} />
+			<ProfileStateContainer>
+				<ProfileState>
+					<h3>98</h3>
+					<h4>Posts</h4>
+				</ProfileState>
+				<h3>.</h3>
+				<ProfileState>
+					<h3>3.5k</h3>
+					<h4>Followers</h4>
+				</ProfileState>
+				<h3>.</h3>
+				<ProfileState>
+					<h3>900</h3>
+					<h4>Followings</h4>
+				</ProfileState>
+			</ProfileStateContainer>
+		</>
+	)
+}
+
+export default ProfileTopSection
--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -3,15 +3,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { IMAGES } from '../../constants'
 import { Marginer } from '../Marginer'
+import ProfileTopSection from './ProfileTopSection'
+import StorieSection from './StorieSection'
+
 const ProfileContainer = styled.div`
 	display: flex;
 	flex: 1;
 	background-color: white;
 	border-left: 2px solid #a9b2e52b;
 	flex-direction: column;
-	/* justify-content: center; */
 	align-items: center;
 `
 const NotifContainer = styled.div`
@@ -42,89 +43,6 @@ const ProfileInfo = styled.div`
 	width: 100%;
 `
 
-const AvatarContainer = styled.div`
-	width: 80px;
-	height: 80px;
-	overflow: hidden;
-	border-radius: 50px;
-	background-image: linear-gradient(
-		90deg,
-		rgba(63, 94, 251, 1) 0%,
-		rgba(252, 70, 107, 1) 100%
-	);
-	padding: 2px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-`
-
-const CircleAvatar = styled.div`
-	overflow: hidden;
-	border-radius: 50px;
-	width: 100%;
-	height: 100%;
-	img {
-		width: 100%;
-		height: 100%;
-	}
-`
-const ProfileName = styled.h3`
-	font-weight: 600;
-	font-size: 16px;
-	color: black;
-	padding: 10px;
-`
-const ProfileDesc = styled.h4`
-	color: gray;
-	font-size: 12px;
-	font-weight: 400;
-`
-
-const StorieContainer = styled.div`
-	text-align: start;
-	width: 100%;
-	h3 {
-		font-weight: 700;
-		font-size: 17px;
-		padding: 10px;
-	}
-`
-const EditButton = styled.button`
-	background-color: black;
-	color: white;
-	border-radius: 5px;
-	width: 75px;
-	height: 35px;
-	cursor: pointer;
-	transition: all 200ms ease-in-out;
-	&:hover {
-		opacity: 0.8;
-	}
-`
-const ProfileStateContainer = styled.div`
-	width: 100%;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-`
-
-const ProfileState = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	padding: 15px;
-	h3 {
-		font-weight: 700;
-		font-size: 17px;
-	}
-	h4 {
-		color: gray;
-		font-size: 12px;
-		font-weight: 500;
-		padding: 5px;
-	}
-`
 const ProfileDescContainer = styled.div`
 	text-align: start;
 	width: 100%;
@@ -141,54 +59,21 @@ const ProfileDescContainer = styled.div`
 	}
 `
 
-const StorieAvatar = styled.div`
-	overflow: hidden;
-	border-radius: 50px;
-	width: 60px;
-	height: 60px;
-	img {
-		width: 100%;
-		height: 100%;
-	}
-`
-const StorieAvatarContainer = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	padding: 10px;
-`
-
-const CreatePostButton = styled.button`
-	width: 100%;
-	height: 40px;
-	color: #fff;
-	border: none;
-	border-radius: 5px;
-	cursor: pointer;
-	font-weight: 700;
-	transition: all 200ms ease-in-out;
-	background: linear-gradient(
-		90deg,
-		rgba(252, 70, 161, 1) 0%,
-		rgba(252, 169, 70, 1) 100%
-	);
-	&:hover {
-		opacity: 0.8;
-	}
-`
+function ProfileInnerSection() {
+	return (
+		<ProfileDescContainer>
+			<h3>Neelesh Chaudhary</h3>
+			<h4>
+				Test your Internet connection bandwidth to locations around the world
+				with this interactive broadband speed test from Ookla. Test your
+				Internet connection bandwidth to locations around the world with this
+				interactive broadband speed test from Ookla.
+			</h4>
+		</ProfileDescContainer>
+	)
+}
 
 function Profile() {
-	const [storieList, setStorieList] = useState([])
-
-	useEffect(() => {
-		const getStorieList = async () => {
-			const response = await axios.get('http://localhost:9000/stories')
-			setStorieList(response.data)
-			console.log(response.data)
-		}
-
-		getStorieList()
-	}, [])
-
 	return (
 		<ProfileContainer>
 			<NotifContainer>
@@ -196,61 +81,10 @@ function Profile() {
 			</NotifContainer>
 			<ProfileContent>
 				<ProfileInfo>
-					<AvatarContainer>
-						<CircleAvatar>
-							<img src={IMAGES.PROFILE_IMAGE} alt='profile' />
-						</CircleAvatar>
-					</AvatarContainer>
-					<ProfileName>Saeed Eyvazy</ProfileName>
-					<ProfileDesc>profile description</ProfileDesc>
-					<Marginer direction='vertical' margin={8} />
-					<EditButton>Edit</EditButton>
+					<ProfileTopSection />
 					<Marginer direction='vertical' margin={20} />
-					<ProfileStateContainer>
-						<ProfileState>
-							<h3>98</h3>
-							<h4>Posts</h4>
-						</ProfileState>
-						<h3>.</h3>
-						<ProfileState>
-							<h3>3.5k</h3>
-							<h4>Followers</h4>
-						</ProfileState>
-						<h3>.</h3>
-						<ProfileState>
-							<h3>900</h3>
-							<h4>Followings</h4>
-						</ProfileState>
-					</ProfileStateContainer>
-					<Marginer direction='vertical' margin={20} />
-					<ProfileDescContainer>
-						<h3>Neelesh Chaudhary</h3>
-						<h4>
-							Test your Internet connection bandwidth to locations around the
-							world with this interactive broadband speed test from Ookla. Test
-							your Internet connection bandwidth to locations around the world
-							with this interactive broadband speed test from Ookla.
-						</h4>
-					</ProfileDescContainer>
-					<StorieContainer>
-						<h3>Your Stories</h3>
-						<StorieAvatarContainer>
-							{storieList.length ? (
-								storieList.map((item) => (
-									<div style={{ padding: '5px' }}>
-										<StorieAvatar>
-											<img src={item.url} alt='storie' />
-										</StorieAvatar>
-									</div>
-								))
-							) : (
-								<h2>Loading...</h2>
-							)}
-						</StorieAvatarContainer>
-						<div style={{ padding: '10px' }}>
-							<CreatePostButton>Create Post</CreatePostButton>
-						</div>
-					</StorieContainer>
+					<ProfileInnerSection />
+					<StorieSection />
 				</ProfileInfo>
 			</ProfileContent>
 		</ProfileContainer>
